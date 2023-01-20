6b5e10b24a9f68d270f09f742017f105ee426f9b
Maxim Tretjakov <maxtretjakov0706@gmail.com>
2023-01-20 11:25:57 +0200

    Initial Commit

--- rozetka_parser/rozetka_parser/spiders/rozetka.py
+++ rozetka_parser/rozetka_parser/spiders/rozetka.py
@@ -5,11 +5,16 @@ from ..items import PhoneSamsungItem
 
 class PhonePider(scrapy.Spider):
     name = 'rozetka'
-    start_urls = ['https://rozetka.com.ua/mobile-phones/c80003/producer=samsung;series=galaxy-s/']
+    start_urls = ['https://rozetka.com.ua/mobile-phones/c80003/']
 
     def parse(self, response):
         for link in response.css('div.goods-tile__colors a::attr(href)'):
             yield response.follow(link, callback=self.parse_phone)
+
+        next_page = response.css('a.button.button--gray.button--medium.pagination__direction.'
+                                 'pagination__direction--forward.ng-star-inserted::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
     def parse_phone(self, response):
         phone_samsung_item = PhoneSamsungItem()
