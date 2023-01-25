508d7d708d8189891b6e89d1a0c3d7700f25abde
Maxim Tretjakov <maxtretjakov0706@gmail.com>
2023-01-25 11:43:34 +0200

    q-techno parse but not in DB

---
+++ rozetka_parser/rozetka_parser/spiders/q-techno.py
@@ -0,0 +1,26 @@
+import scrapy
+import html
+# from ..items import PhoneSamsungItem
+
+
+class PhoneSpider(scrapy.Spider):
+    name = 'q-techno'
+    start_urls = ['https://q-techno.com.ua/ru/shop/telefony-mobil-nye']
+
+    def parse(self, response):
+        for link in response.css('div.product-name.m_b15 a::attr(href)'):
+            yield response.follow(link, callback=self.parse_phone)
+
+        next_page = response.css('#catalog-listing > div > div.toolbar-bottom.m_b50.clearfix > div > '
+                                 'div.pager.clearfix > div > ol > li:nth-child(6) > a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_phone(self, response):
+        #  phone_samsung_item = PhoneSamsungItem()
+        phone_samsung_item = {
+            'model': response.css('h1.dynamic-data-name::text').get(),
+            'price': html.unescape(response.css('span.price::text').get()),
+            'url': response.css('head > meta:nth-child(9)::attr(content)').get()
+        }
+        yield phone_samsung_item
